539e7a7361fc1eb96af3245ae2730a023959aeed
Beka Gelashvili <17614392+ltAldoRaine@users.noreply.github.com>
2022-03-01 13:46:16 +0400

    Update RadioAccessTechnology.swift

--- Sources/SharedFramework/Reachability/RadioAccessTechnology.swift
+++ Sources/SharedFramework/Reachability/RadioAccessTechnology.swift
@@ -9,85 +9,85 @@ public class CTTelephonyNetworkInfo {
 }
 
 public class RadioAccessTechnology {
-//    public static let shared = RadioAccessTechnology()
-//
-//    fileprivate let telephonyNetworkInfo = CTTelephonyNetworkInfo()
-//    fileprivate(set) var reachability: Reachability?
-//    public weak var delegate: RadioAccessTechnologyNotifierDelegate?
-//
-//    fileprivate init() {
-//        defer {
-//            NotificationCenter.default.addObserver(
-//                self,
-//                selector: #selector(radioAccessTechnologyDidChange),
-//                name: Notification.Name.CTServiceRadioAccessTechnologyDidChange,
-//                object: nil)
-//            NotificationCenter.default.addObserver(
-//                self,
-//                selector: #selector(reachabilityChanged),
-//                name: ReachabilityChangedNotification,
-//                object: nil)
-//        }
-//
-//        reachability = Reachability()
-//        try? reachability?.startNotifier()
-//    }
-//
-//    deinit {
-//        NotificationCenter.default.removeObserver(self)
-//    }
-//
-//    public fileprivate(set) var currentState: State = .unknown {
-//        didSet {
-//            guard currentState != oldValue else {return}
-//
-//            NotificationCenter.default.post(name: RadioAccessTechnologyStateDidChange, object: nil)
-//            delegate?.didChange(to: currentState)
-//        }
-//    }
-//
-//    @objc fileprivate func radioAccessTechnologyDidChange() {
-//        reachabilityChanged()
-//    }
-//
-//    @objc fileprivate func reachabilityChanged() {
-//        currentState = getCurrentStateByReachibility()
-//    }
-//
-//    fileprivate var currentStateOnWWAN: State {
-//        guard let serviceCurrentRadioAccessTechnology = telephonyNetworkInfo.serviceCurrentRadioAccessTechnology else {
-//            return .unknown
-//        }
-//
-//        guard let radioAccessTechnology = serviceCurrentRadioAccessTechnology.values.first else {
-//            return .unknown
-//        }
-//
-//        switch radioAccessTechnology {
-//        case CTRadioAccessTechnologyGPRS,
-//             CTRadioAccessTechnologyEdge,
-//             CTRadioAccessTechnologyCDMA1x: return .edge
-//        case CTRadioAccessTechnologyWCDMA,
-//             CTRadioAccessTechnologyHSDPA,
-//             CTRadioAccessTechnologyHSUPA,
-//             CTRadioAccessTechnologyCDMAEVDORev0,
-//             CTRadioAccessTechnologyCDMAEVDORevA,
-//             CTRadioAccessTechnologyCDMAEVDORevB,
-//             CTRadioAccessTechnologyeHRPD: return ._3G
-//        case CTRadioAccessTechnologyLTE: return .lte
-//        default: return .unknown
-//        }
-//    }
-//
-//    fileprivate func getCurrentStateByReachibility() -> State {
-//        guard let status = reachability?.currentReachabilityStatus else {return .unknown}
-//
-//        switch status {
-//        case .reachableViaWiFi: return .wifi
-//        case .reachableViaWWAN: return currentStateOnWWAN
-//        default: return .unknown
-//        }
-//    }
+   public static let shared = RadioAccessTechnology()
+
+   fileprivate let telephonyNetworkInfo = CTTelephonyNetworkInfo()
+   fileprivate(set) var reachability: Reachability?
+   public weak var delegate: RadioAccessTechnologyNotifierDelegate?
+
+   fileprivate init() {
+       defer {
+           NotificationCenter.default.addObserver(
+               self,
+               selector: #selector(radioAccessTechnologyDidChange),
+               name: Notification.Name.CTServiceRadioAccessTechnologyDidChange,
+               object: nil)
+           NotificationCenter.default.addObserver(
+               self,
+               selector: #selector(reachabilityChanged),
+               name: ReachabilityChangedNotification,
+               object: nil)
+       }
+
+       reachability = Reachability()
+       try? reachability?.startNotifier()
+   }
+
+   deinit {
+       NotificationCenter.default.removeObserver(self)
+   }
+
+   public fileprivate(set) var currentState: State = .unknown {
+       didSet {
+           guard currentState != oldValue else {return}
+
+           NotificationCenter.default.post(name: RadioAccessTechnologyStateDidChange, object: nil)
+           delegate?.didChange(to: currentState)
+       }
+   }
+
+   @objc fileprivate func radioAccessTechnologyDidChange() {
+       reachabilityChanged()
+   }
+
+   @objc fileprivate func reachabilityChanged() {
+       currentState = getCurrentStateByReachibility()
+   }
+
+   fileprivate var currentStateOnWWAN: State {
+       guard let serviceCurrentRadioAccessTechnology = telephonyNetworkInfo.serviceCurrentRadioAccessTechnology else {
+           return .unknown
+       }
+
+       guard let radioAccessTechnology = serviceCurrentRadioAccessTechnology.values.first else {
+           return .unknown
+       }
+
+       switch radioAccessTechnology {
+       case CTRadioAccessTechnologyGPRS,
+            CTRadioAccessTechnologyEdge,
+            CTRadioAccessTechnologyCDMA1x: return .edge
+       case CTRadioAccessTechnologyWCDMA,
+            CTRadioAccessTechnologyHSDPA,
+            CTRadioAccessTechnologyHSUPA,
+            CTRadioAccessTechnologyCDMAEVDORev0,
+            CTRadioAccessTechnologyCDMAEVDORevA,
+            CTRadioAccessTechnologyCDMAEVDORevB,
+            CTRadioAccessTechnologyeHRPD: return ._3G
+       case CTRadioAccessTechnologyLTE: return .lte
+       default: return .unknown
+       }
+   }
+
+   fileprivate func getCurrentStateByReachibility() -> State {
+       guard let status = reachability?.currentReachabilityStatus else {return .unknown}
+
+       switch status {
+       case .reachableViaWiFi: return .wifi
+       case .reachableViaWWAN: return currentStateOnWWAN
+       default: return .unknown
+       }
+   }
 }
 
 extension RadioAccessTechnology {
